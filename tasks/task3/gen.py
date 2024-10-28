057ebf9bcfd75b16d7d3018e707e2230d4e7bc64
Nazar <39676526+NazarJKostiv@users.noreply.github.com>
2024-10-28 12:27:36 +0200

    task3: changed gen to return randon value

--- tasks/task3/gen.py
+++ tasks/task3/gen.py
@@ -2,17 +2,11 @@ import random
 
 
 def get_one_value(lst):
-    seen = set()
-    index = 0
-    random.shuffle(lst)
     while True:
-        if lst[index] not in seen:
-            seen.add(lst[index])
-            yield lst[index]
-        index = (index + 1) % len(lst)
-        if index == 0:
-            seen.clear()
-            random.shuffle(lst)
+        random.shuffle(lst)
+        for item in lst:
+            yield item
+
 
 
 lst = [2, 4, 5, 1]
