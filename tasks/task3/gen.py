d26b995e40f3bf0c1e55b7d74c35702290344bd1
Nazar <39676526+NazarJKostiv@users.noreply.github.com>
2024-10-28 12:40:33 +0200

    task3: changed gen to return randon value

--- tasks/task3/gen.py
+++ tasks/task3/gen.py
@@ -1,25 +1,28 @@
 import random
 
 
+def get_one_value_no_dplc(lst):
+    seen = set()
+    while True:
+        random.shuffle(lst)
+        for item in lst:
+            if item not in seen:
+                seen.add(item)
+                yield item
+        seen.clear()
+
 def get_one_value(lst):
     while True:
         random.shuffle(lst)
         for item in lst:
             yield item
 
-
-
-lst = [2, 4, 5, 1]
+lst = [2, 4, 5, 2]
 gen = get_one_value(lst)
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
+gen2 = get_one_value_no_dplc(lst)
+
+for _ in range(10):
+    print(next(gen))
+print("-"*24)
+for _ in range(10):
+    print(next(gen2))
